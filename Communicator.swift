4cc404a477d5654f988b5e6f503ae490ae93f171
TonyDong <yidong@wustl.edu>
2014-12-07 00:39:05 -0600

    almost done

--- Communicator.swift
+++ Communicator.swift
@@ -7,10 +7,11 @@
 //
 
 import Foundation
+import CoreLocation
 
 class Communicator {
-    let baseURL = "ec2-54-148-7-254.us-west-2.compute.amazonaws.com/"
-    let setTempMethod = "settemprature"
+    let baseURL = "http://54.148.7.254/"
+    let setTempMethod = "settemperature"
     let setLocMethod = "setlocation"
     let getDeviceInfoMethod = "getdeviceinfo"
     let getDeviceDataMethod = "getdevicedata"
@@ -20,59 +21,115 @@ class Communicator {
     var isController : Bool
     var sensiInfoAry : [SensiInfo]
     var pattern : Dictionary<Int, (Float, Float)>
+    var locationManager : CLLocationManager
+    
+    lazy var data = NSMutableData()
     
     init () {
-        deviceID = 123
-        userID = 123
+        deviceID = 1
+        userID = 1
         isController = false
+        sensiInfoAry = Array<SensiInfo>()
+        pattern = Dictionary<Int, (Float, Float)>()
+        
+        locationManager = CLLocationManager()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestAlwaysAuthorization()
+        locationManager.startUpdatingLocation()
+        
+        self.getSensiInfo()
+        
     }
     
-    func getRequest(method: String, param: Dictionary<String, String>) -> NSURLRequest {
+    func getRequest(method: String, param: Dictionary<String, String>) -> NSURL {
         var result = baseURL + method + ".php?"
         for key in param.keys {
             result = result + key + "=" + param[key]! + "&"
         }
+        
+        result.substringToIndex(result.endIndex.predecessor())
+
         var toReturnURL = NSURL(string: result)
+        println(result)
         var toReturn = NSURLRequest(URL: toReturnURL!)
-        return toReturn
+        return toReturnURL!
     }
     
     
     // Assuming the passed in Dictionary with Key named "temperature"
     // Value is the desired temperature to set the Sensi to
     func setTemp(temperatureToSetDict : Dictionary<String, Int>) -> Bool {
-        var temp = temperatureToSetDict["temperature"]
+        var temp = temperatureToSetDict["temperature"]!
         var toPassIn: [String: String] = ["userkey": "\(userID)", "devicekey": "\(deviceID)", "setpoint": "\(temp)"]
         
-        var request = getRequest(setTempMethod, param: toPassIn)
+        let request = getRequest(setTempMethod, param: toPassIn)
 
         var toReturn = false
         
-        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) {(response, data, error) in
-            println(NSString(data: data, encoding: NSUTF8StringEncoding))
+        
+//        var connection: NSURLConnection = NSURLConnection(request: request, delegate: self, startImmediately: false)!
+//        
+//        connection.start()
+//        let url = NSURL(string: "http://54.148.7.254/settemperature.php?devicekey=1&setpoint=78&userkey=1")
+        
+        let task = NSURLSession.sharedSession().dataTaskWithURL(request) {(data, response, error) in
+            
+            var json = JSON(data: data)
+            
+            if (json["result"].integerValue == 1) {
+                toReturn = true
+            }
         }
+        
+        task.resume()
         
         return toReturn
     }
     
     
     // Assume Dictinary["location", tuple (Latitude, Longitude)]
-    func setLocation(locationToSend : Dictionary<String, (Double, Double)>) -> Bool{
+    func setLocation() -> Bool{
         return true
     }
     
+    func getUserLocation() -> (Double, Double) {
+        
+        return (0.0, 0.0)
+    }
+    
+    func getWeatherForZip(zipCpde: NSString) -> Dictionary<String, String> {
+//        var weatherBaseString = "http://api.wunderground.com/api/163809ff8d2f1239/conditions/q/CA/\(zipCpde).json"
+        var weatherBaseURL = NSURL(string: "http://api.wunderground.com/api/163809ff8d2f1239/conditions/q/CA/\(zipCpde).json")
+        
+        var toReturn = Dictionary<String, String>()
+        
+       
+        let task = NSURLSession.sharedSession().dataTaskWithURL(weatherBaseURL!) {(data, response, error) in
+            
+             var json = JSON(data: data)
+            
+            toReturn["temperature"] = json["temperature_string"].stringValue
+            toReturn["temp_f"] = json["temp_f"].stringValue
+            
+        }
+        
+        task.resume()
+        
+        return toReturn
+    }
+    
+    
     func getSensiInfo() -> ([SensiInfo], userIsController: Bool) {
         var toPassIn: [String: String] = ["userkey": "\(userID)", "devicekey": "\(deviceID)"]
-        var request = getRequest(setTempMethod, param: toPassIn)
+        var request = getRequest(getDeviceInfoMethod, param: toPassIn)
         
         var toReturnAry = Array<SensiInfo>()
         var toReturnBool = false
         
-        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) {(response, data, error) in
-            println(NSString(data: data, encoding: NSUTF8StringEncoding))
-            let json = JSON(data: data)
-            var upperBound = json["count"].integerValue!
-            for i in 1...upperBound {
+        let task = NSURLSession.sharedSession().dataTaskWithURL(request) {(data, response, error) in
+            var json = JSON(data: data)
+            var len = json["count"].integerValue
+            for i in 0...(len!-1) {
                 toReturnAry.append(SensiInfo(index: i, json: json))
             }
             if toReturnAry[0].isController {
@@ -80,15 +137,53 @@ class Communicator {
                 self.isController = true
             }
             self.sensiInfoAry = toReturnAry
+            
         }
+        
+        task.resume()
+        
+
         return (toReturnAry, toReturnBool)
     }
     
-    func getPattern(startTime: NSDate, endTime: NSDate) -> Dictionary<Int, (Float, Float)> {
+    func getPattern(startTime: NSDate, endTime: NSDate) -> [DeviceData] {
+        
+        var toReturn = Array<DeviceData>()
+        
+        
+        var toPassIn: [String: String] = ["devicekey": "\(deviceID)", "starttime": "\(startTime.timeIntervalSince1970)", "endtime": "\(endTime.timeIntervalSince1970)"]
+        var request = getRequest(getDeviceDataMethod, param: toPassIn)
+        println(request)
+        
+        let task = NSURLSession.sharedSession().dataTaskWithURL(request) {(data, response, error) in
+            var json = JSON(data: data)
+            var len = json["count"].integerValue
+            println(json["data"][0]["sugg_temperature"])
+            for var i = 0; i < len; i++ {
+                toReturn.append(DeviceData(stamp: json["data"][i]["timestamp_utc"].integerValue!, suggest: json["data"][i]["sugg_temperature"].floatValue!, sensed: json["data"][i]["sensed_temperature"].floatValue!))
+            }
+            
+        }
+        
+        task.resume()
+        
+        return toReturn
         
     }
     
     
+}
+
+class DeviceData {
+    var timeStamp : Int
+    var suggestedTemp: Float
+    var sensedTemp: Float
+    
+    init(stamp: Int, suggest: Float, sensed: Float) {
+        timeStamp = stamp
+        suggestedTemp = suggest
+        sensedTemp = sensed
+    }
 }
 
 class SensiInfo {
@@ -103,20 +198,33 @@ class SensiInfo {
     var status : String
     var siteKey : Int
     var siteName : String
-    var siteTemperature : Float
+//    var siteTemperature : Float
     
     init(index : Int, json : JSON) {
-        deviceKey = json["devices"][index]["devicekey"].integerValue!
+        deviceKey = 1
         deviceName = json["devices"][index]["devicename"].stringValue!
         isController = json["devices"][index]["privilageLevel"].boolValue
-        roomTemperature = json["devices"][index]["roomtemperature"].floatValue!
-        setPointTemperature  = json["devices"][index]["setpointtemperature"].integerValue!
-        humidity = json["devices"][index]["humidity"].floatValue!
-        batteryLevel = json["devices"][index]["batterylevel"].integerValue!
-        mode = json["devices"][index]["mode"].stringValue!
-        status = json["devices"][index]["status"].stringValue!
+        roomTemperature = json["devices"][index]["recent_roomtemperature"].floatValue!
+        setPointTemperature  = json["devices"][index]["recent_setpointtemperature"].integerValue!
+        humidity = json["devices"][index]["recent_humidity"].floatValue!
+        batteryLevel = json["devices"][index]["recent_batterylevel"].integerValue!
+        mode = json["devices"][index]["recent_mode"].stringValue!
+        status = json["devices"][index]["recent_status"].stringValue!
         siteKey = json["devices"][index]["sitekey"].integerValue!
         siteName = json["devices"][index]["sitename"].stringValue!
-        siteTemperature = json["devices"][index]["sitetemperature"].floatValue!
+//        siteTemperature = json["devices"][index]["sitetemperature"].floatValue!
+    }
+    
+    func printMe() {
+        var namespace = "deviceKey: " + String(deviceKey) + ", deviceName: " + deviceName
+        println("**********" + namespace + "*********")
+        println("isController? \(isController)")
+        println("Room Temperature: \(roomTemperature), Set to: \(setPointTemperature)")
+        println("Humidity: \(humidity)")
+        println("batteryLevel: \(batteryLevel), mode: \(mode)")
+        println("status: \(status)")
+        println("siteKey: \(siteKey), siteName: \(siteName)")
+//        println("siteTemperature: \(siteTemperature)")
+        
     }
 }
--- SensiCommunicator/ViewController.swift
+++ SensiCommunicator/ViewController.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var magicButton: UIButton!
+    
+    let communicator = Communicator()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func magicButtonPressed(sender: AnyObject) {
+        var toPassIn: [String: Int] = ["temperature": 78]
+        communicator.setTemp(toPassIn)
+        for item in communicator.sensiInfoAry {
+            item.printMe()
+        }
+        
+        communicator.getPattern(NSDate(timeIntervalSince1970: 12312), endTime: NSDate())
+    }
 
 }
 
